4c4470958383bc908049ef2152fed50119739c53
your_username <adrianclintbullo@gmail.com>
2026-07-01 14:17:04 +0800

    added 3rd number

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 function calculateSum(){
     const num1 = document.getElementById('num1').value;
-     const num2 = document.getElementById('num2').value;
-     
-     const number1 = parseFloat(num1);
-      const number2 = parseFloat(num2);
+    const num2 = document.getElementById('num2').value;
+     const num3 = document.getElementById('num3').value;
 
-      //Calculate Sum
-      const sum =number1 + number2;
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
+    
 
-      // display the result
-      document.getElementById('result').textContent='The sum is  ' +sum;
+    //calculate sun
+    const sum =number1 + number2 + number3;
+    
+    //display the result
+    document.getElementById('result').textContent='The sum is ' + sum;
+}
+function calculateMult(){
+    const num1 = document.getElementById('num1').value;
+    const num2 = document.getElementById('num2').value;
+     const num3 = document.getElementById('num3').value;
+
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    const number3 = parseFloat(num3);
+
+    //calculate sun
+    const sum =number1 * number2 * number3;
+    
+    //display the result
+    document.getElementById('result').textContent='The sum is ' + sum;
 }
